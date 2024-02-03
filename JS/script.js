d24d1d46f1c692fda1e66c2f091d9ce25ad22d63
Salma Qassem <salma99kasem@gmail.com>
2024-02-03 19:54:05 +0200

    added responsive style

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,24 @@ const playButton = document.querySelector(".play-button");
 const homePage = document.querySelector(".homepage");
 const gamePage = document.querySelector(".game");
 const gameImages = document.querySelectorAll(".game-images .img-container ");
+const infoIcon = document.querySelector(".bottom.icon");
+
+const animateInfo = () => {
+  infoIcon.classList.add("show");
+  infoIcon.addEventListener("animationend", () => {
+    setTimeout(() => {
+      infoIcon.classList.remove("show");
+      infoIcon.classList.add("hide");
+    }, 1000);
+  });
+};
 playButton.addEventListener("click", () => {
   homePage.classList.add("hide");
   homePage.addEventListener("animationend", () => {
     homePage.classList.remove("hide");
     homePage.style.visibility = "hidden";
     gamePage.style.visibility = "visible";
+    animateInfo();
   });
 });
 gameImages.forEach((image) => {
@@ -16,8 +28,15 @@ gameImages.forEach((image) => {
     image.classList.add("colored");
     image.addEventListener("animationend", () => {
       image.classList.remove("clicked");
-      //image.style.visibility = "hidden";
-      //gamePage.style.visibility = "visible";
+    });
+    const text = image.querySelector(".card-text");
+    text.classList.add("show");
+    text.addEventListener("animationend", () => {
+      text.classList.remove("show");
     });
   });
+});
+infoIcon.addEventListener("click", () => {
+  infoIcon.classList.remove("hide");
+  animateInfo();
 });
