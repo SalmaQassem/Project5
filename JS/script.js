33d3fa90afdd9f68eb1be75da39271c277e2f4c1
Salma Qassem <salma99kasem@gmail.com>
2024-02-04 23:19:10 +0200

    added success

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,12 @@
 const playButton = document.querySelector(".play-button");
 const homePage = document.querySelector(".homepage");
 const gamePage = document.querySelector(".game");
-const gameImages = document.querySelectorAll(".game-images .img-container ");
+const gameImages = document.querySelectorAll(".game-images .img-container");
 const infoIcon = document.querySelector(".bottom.icon");
-
+const scoreElement = document.querySelector(".scoreWrapper .score");
+const successModal = document.querySelector(".success-card");
+const overlay = document.querySelector(".overlay");
+let counter = 0;
 const animateInfo = () => {
   infoIcon.classList.add("show");
   infoIcon.addEventListener("animationend", () => {
@@ -14,6 +17,7 @@ const animateInfo = () => {
   });
 };
 playButton.addEventListener("click", () => {
+  document.querySelector("#start-audio").play();
   homePage.classList.add("hide");
   homePage.addEventListener("animationend", () => {
     homePage.classList.remove("hide");
@@ -24,6 +28,7 @@ playButton.addEventListener("click", () => {
 });
 gameImages.forEach((image) => {
   image.addEventListener("click", () => {
+    document.querySelector(`audio[id="${image.dataset.id}"]`).play();
     image.classList.add("clicked");
     image.classList.add("colored");
     image.addEventListener("animationend", () => {
@@ -34,9 +39,37 @@ gameImages.forEach((image) => {
     text.addEventListener("animationend", () => {
       text.classList.remove("show");
     });
+    counter += 1;
+    scoreElement.textContent = `${counter}/${gameImages.length}`;
+    document
+      .querySelector(":root")
+      .style.setProperty("--width", `${(100 / gameImages.length) * counter}%`);
+    if (counter === gameImages.length) {
+      const text = document.querySelector(".text-card .score-text");
+      text.textContent = `${counter}/${gameImages.length}`;
+      successModal.classList.add("show");
+      overlay.classList.add("show");
+    }
   });
 });
 infoIcon.addEventListener("click", () => {
   infoIcon.classList.remove("hide");
   animateInfo();
+});
+/*window.addEventListener("click", (e) => {
+  if (
+    !successModal.contains(e.target) &&
+    successModal.classList.contains("show")
+  ) {
+    successModal.classList.remove("show");
+    setTimeout(() => {
+      overlay.classList.remove("show");
+    }, 400);
+  }
+});*/
+document.querySelector(".closeModal").addEventListener("click", () => {
+  successModal.classList.remove("show");
+  setTimeout(() => {
+    overlay.classList.remove("show");
+  }, 400);
 });
